1e514e7314cd360e5b8905365662a3e6a371a54e
mohammed <M.abahussain@lean.sa>
2023-05-21 18:27:14 +0300

    updated

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -63,7 +63,7 @@ export default function Team() {
                     
                     <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                         <div className="max-w-2xl">
-                            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
+                            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our HEROS</h2>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
                             
                             </p>
